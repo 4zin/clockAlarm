bab3fe735733da2e0c532656ed20343017484d03
Camilo Rodelo <camilo_jrl@hotmail.com>
2024-02-15 17:39:54 -0500

    re builded

--- app/components/Clock.tsx
+++ app/components/Clock.tsx
@@ -5,12 +5,36 @@ import { useState, useEffect } from "react";
 export const Clock = () => {
 
 
-    const [clock, setClock] = useState<string>('');
+    const [digitalHour, setDigitalHour] = useState<string>('');
+    const [digitalMinute, setDigitalMinute] = useState<string>('');
+    const [digitalSeconds, setDigitalSeconds] = useState<string>('');
+    const [ampm, setAmpm] = useState<string>('');
 
     useEffect(() => {
         const updateClock = () => {
-            let time = new Date().toLocaleTimeString();
-            return setClock(time);
+            let date = new Date()
+
+            let HH: number | string = date.getHours()
+            let MM: number | string = date.getMinutes()
+            let SS: number | string = date.getSeconds()
+            let ampm;
+
+            if (HH >= 12) {
+                HH -= 12
+                ampm = "PM"
+            } else {
+                ampm = "AM"
+            }
+
+            if (HH === 0) HH = 12
+            if (HH < 10) HH = `0${HH}`;
+            if (MM < 10) MM = `0${MM}`;
+            if (SS < 10) SS = `0${SS}`
+
+            setDigitalHour(HH.toString())
+            setDigitalMinute(MM.toString())
+            setDigitalSeconds(SS.toString())
+            setAmpm(ampm)
         }
 
         setInterval(updateClock, 1000)
@@ -22,7 +46,7 @@ export const Clock = () => {
     return (
         <div className="flex flex-col gap-3 items-center">
             <h1 className="text-accents-100 text-2xl">Music Clock Alarm</h1>
-            {clock && <p className="text-3xl text-accents-100">{clock}</p>}
+            {digitalHour && <p className="text-3xl text-accents-100">{digitalHour}:{digitalMinute}:{digitalSeconds} {ampm}</p>}
         </div>
     )
 }
--- app/components/SetAlarm.tsx
+++ app/components/SetAlarm.tsx
@@ -4,23 +4,56 @@ import { useState } from 'react'
 
 export const SetAlarm = () => {
 
-    const [hour, setHour] = useState<number[]>(Array(24).fill(0))
-    const [minute, setMinute] = useState<number[]>(Array(60).fill(0))
+    const [hour, setHour] = useState<string>('Hour')
+    const [minutes, setMinutes] = useState<string>('Minutes')
+    const [amPmOptions, setAmPmOptions] = useState<string>('AM-PM')
+
+
+    const fixNumber = (value: number[]): (string | number)[] => {
+        return value.map(hour => (hour < 10 ? '0' + hour : hour));
+    };
+
+    const hourNumber = fixNumber(Array.from(Array(13).keys()))
+    const minuteNumber = fixNumber(Array.from(Array(60).keys()))
+
+    const hourHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setHour(event.target.value)
+    }
+    const minuteHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setMinutes(event.target.value)
+    }
+    const amPmHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setAmPmOptions(event.target.value)
+    }
+
 
     return (
-        <div className='flex gap-4'>
-            <select name="" id="">
-                <option value="" disabled>Hour</option>
-                {
-                    hour.map((_, index) => <option key={index} value={index}>{index}</option>)
-                }
-            </select>
-            {/* <select name="" id="">
-                <option value="">Minute</option>
-                {
-                    minute.map((_, index) => <option key={index} value={index}>{index}</option>)
-                }
-            </select> */}
+        <div className='flex flex-col'>
+            <div className='flex gap-4'>
+                <select value={hour} onChange={hourHandler}>
+                    <option disabled value="Hour">Hour</option>
+                    {
+                        hourNumber.map((hour, index) => <option key={index} value={hour}>{hour}</option>)
+                    }
+                </select>
+
+                <select value={minutes} onChange={minuteHandler}>
+                    <option disabled value="Minutes">Minutes</option>
+                    {
+                        minuteNumber.map((minute, index) => <option key={index} value={minute}>{minute}</option>)
+                    }
+                </select>
+
+                <select value={amPmOptions} onChange={amPmHandler}>
+                    <option disabled value="AM-PM">AM-PM</option>
+                    <option value="AM">AM</option>
+                    <option value="PM">PM</option>
+                </select>
+            </div>
+
+            <div className='flex justify-center'>
+                <button>Set Alarm</button>
+            </div>
         </div>
     )
 }
