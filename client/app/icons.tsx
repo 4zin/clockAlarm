c7385e4612ac11288ae431aa2824d311fd34f2c2
Camilo Rodelo <camilo_jrl@hotmail.com>
2024-03-05 20:50:40 -0500

    Changed structure

---
+++ client/app/icons.tsx
@@ -0,0 +1,62 @@
+export function MuteIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-volume-3" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6 15h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l3.5 -4.5a.8 .8 0 0 1 1.5 .5v14a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5" /><path d="M16 10l4 4m0 -4l-4 4" /></svg>
+
+    )
+}
+
+export function PalaIcon() {
+    return (
+        <svg width="30" height="30" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="3" y="11" width="2" height="1" fill="#3E1E00" />
+            <rect x="3" y="10" width="1" height="1" fill="#6B2000" />
+            <rect x="4" y="10" width="1" height="1" fill="#A05332" />
+            <rect x="5" y="10" width="1" height="1" fill="#3E1E00" />
+            <rect x="6" y="9" width="1" height="1" fill="#3E1E00" />
+            <rect x="7" y="8" width="1" height="1" fill="#3E1E00" />
+            <rect x="8" y="7" width="1" height="1" fill="#3E1E00" />
+            <rect x="9" y="6" width="1" height="1" fill="#3E1E00" />
+            <rect x="4" y="9" width="1" height="1" fill="#6A2000" />
+            <rect x="5" y="8" width="1" height="1" fill="#6A2000" />
+            <rect x="6" y="7" width="1" height="1" fill="#6A2000" />
+            <rect x="7" y="6" width="1" height="1" fill="#6A2000" />
+            <rect x="8" y="5" width="1" height="1" fill="#6A2000" />
+            <rect x="5" y="9" width="1" height="1" fill="#A05332" />
+            <rect x="6" y="8" width="1" height="1" fill="#A05332" />
+            <rect x="7" y="7" width="1" height="1" fill="#A05332" />
+            <rect x="8" y="6" width="1" height="1" fill="#A05332" />
+            <rect x="9" y="5" width="1" height="1" fill="#1A1919" />
+            <rect x="8" y="4" width="1" height="1" fill="black" />
+            <rect x="10" y="6" width="1" height="1" fill="#272626" />
+            <rect x="11" y="7" width="1" height="1" fill="black" />
+            <rect x="12" y="6" width="1" height="1" fill="black" />
+            <rect x="13" y="5" width="1" height="1" fill="black" />
+            <rect x="14" y="4" width="1" height="1" fill="black" />
+            <rect x="14" y="1" width="1" height="3" fill="black" />
+            <rect x="13" width="1" height="1" fill="#232323" />
+            <rect x="10" width="2" height="1" fill="#232323" />
+            <rect x="12" width="1" height="1" fill="#232323" />
+            <rect x="9" y="1" width="1" height="1" fill="#232323" />
+            <rect x="8" y="2" width="1" height="1" fill="#232323" />
+            <rect x="7" y="3" width="1" height="1" fill="#232323" />
+            <rect x="10" y="1" width="1" height="1" fill="#232323" />
+            <rect x="9" y="2" width="1" height="1" fill="#232323" />
+            <rect x="8" y="3" width="1" height="1" fill="#232323" />
+            <rect x="9" y="4" width="1" height="1" fill="#797979" />
+            <rect x="9" y="3" width="2" height="1" fill="#797979" />
+            <rect x="10" y="2" width="1" height="1" fill="#797979" />
+            <rect x="11" y="2" width="1" height="1" fill="#797979" />
+            <rect x="11" y="1" width="3" height="1" fill="#797979" />
+            <rect x="13" y="2" width="1" height="2" fill="#797979" />
+            <rect x="12" y="4" width="1" height="1" fill="#797979" />
+            <rect x="11" y="5" width="1" height="1" fill="#797979" />
+            <rect x="11" y="6" width="1" height="1" fill="#888888" />
+            <rect x="12" y="2" width="1" height="1" fill="#3D3D3D" />
+            <rect x="11" y="3" width="2" height="1" fill="#3D3D3D" />
+            <rect x="10" y="4" width="2" height="1" fill="#3D3D3D" />
+            <rect x="10" y="5" width="1" height="1" fill="#3D3D3D" />
+            <rect x="13" y="4" width="1" height="1" fill="#232323" />
+            <rect x="12" y="5" width="1" height="1" fill="#232323" />
+        </svg>
+    )
+}
