1cb3fe2322e01365d638f5aac2246bdb484e0a78
Camilo Rodelo <camilo_jrl@hotmail.com>
2024-03-20 09:46:30 -0500

    Types modified for contexts (Alarm and Clock)

--- client/app/types.ts
+++ client/app/types.ts
@@ -1,20 +1,28 @@
-export type ContextAlarmProps = {
+import { Dispatch, SetStateAction } from "react";
+
+export type ContextClockProps = {
   digitalHour: string;
   digitalMinute: string;
   digitalSeconds: string;
   ampm: string;
+}
+
+export type AlarmContextProps = {
   hour: string;
   minutes: string;
   amPmOptions: string;
   alarmTime: AlarmTime;
-  setAlarmHandler: () => void;
-  silenceAlarm: () => void;
+  // setAlarmHandler: () => void;
+  setAlarmTime: Dispatch<SetStateAction<AlarmTime>>;
+  // silenceAlarm: () => void;
+  setAlarmConfig: (alarmConfig: boolean) => void;
   hourNumber: (string | number)[];
   minuteNumber: (string | number)[];
-  hourHandler: (event: React.ChangeEvent<HTMLSelectElement>) => void
-  minuteHandler: (event: React.ChangeEvent<HTMLSelectElement>) => void
-  amPmHandler: (event: React.ChangeEvent<HTMLSelectElement>) => void
-  alarmConfig: boolean
+  hourHandler: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+  minuteHandler: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+  amPmHandler: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+  alarmConfig: boolean;
+  alarmTimeString: string;
 }
 
 export type AlarmTime = {
