c264cbc70439e46d8c2df4a6988945a4f0eb420e
Camilo Rodelo <camilo_jrl@hotmail.com>
2024-04-01 14:04:06 -0500

    constant change

--- client/app/constants.ts
+++ client/app/constants.ts
@@ -1,6 +1,8 @@
-function isString(value: unknown): asserts value is string {
-  if (typeof value !== "string") throw new Error("value is not string");
-}
+// function isString(value): asserts value is string {
+//   if (typeof value !== "string") throw new Error("value is not string");
+// }
 
-isString(process.env.SPOTIFY_CLIENT_ID);
+// isString(process.env.SPOTIFY_CLIENT_ID);
 export const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+
+console.log(process.env.SPOTIFY_CLIENT_ID);
